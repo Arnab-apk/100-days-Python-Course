e94cdd55e9dd20a7c0038e55c78891524da8f084
Arnab-apk <176360399+Arnab-apk@users.noreply.github.com>
2025-06-24 22:38:09 +0530

    done

--- day5/highest_marks.py
+++ day5/highest_marks.py
@@ -1,2 +1,15 @@
 student_score=[150,130,100,250,350,450,600,300]
-for 
+# total=sum(student_score)
+# print(total)
+sum=0
+for score in student_score:
+    sum+=score
+print(sum)       
+
+# print(max(student_score))
+#max functionality
+max=0
+for maxi in student_score:
+    if(maxi>max):
+        max=maxi
+print(max)
