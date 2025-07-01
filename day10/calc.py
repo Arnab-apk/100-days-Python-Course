74638c12e7b41bed60bf915785d20c8c9fa259e9
Arnab-apk <176360399+Arnab-apk@users.noreply.github.com>
2025-07-01 21:41:51 +0530

    done

--- day10/calc.py
+++ day10/calc.py
@@ -1,15 +1,13 @@
+import art
+print(art.art)
 def add(n1,n2):
     return n1+n2
-
 def substract(n1,n2):
     return n1-n2
-
 def multiply(n1,n2):
     return n1*n2
-
 def divide(n1,n2):
     return n1/n2
-
 
 operations={
     "+":add,
@@ -17,8 +15,30 @@ operations={
     "*":multiply,
     "/":divide
 }
+#accesing the function using key 
+# print(operations["*"](4,5))
+def calculator():
+    should_accumulate=True
+    num1=float(input("What is the first number?: "))
+    while(should_accumulate):
+        
+        for operation in operations:
+            print(operation)
 
-print(operations["*"](4,5))
+        operation_sym=input("Pick an operation: ")
+        num2=float(input("What is the next numbers?: "))
+        answer=operations[operation_sym](num1,num2)
+        print(f"{num1} {operation_sym} {num2} = {operations[operation_sym](num1,num2)}")
+        choice=input(f"Type 'y' to continue calculating with {answer} , or type 'n' to start a new calculation: ")
+        if(choice=="y"):
+            num1=answer
+        else:
+            should_accumulate=False
+            print("\n"*20)
+            #tail recursion
+            calculator()
+            
+calculator()
 
 
 
