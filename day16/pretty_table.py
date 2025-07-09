d468c75e3106976f6a0edfa8f19651898d1ae0a7
Arnab-apk <176360399+Arnab-apk@users.noreply.github.com>
2025-07-09 12:29:27 +0530

    done

--- day16/pretty_table.py
+++ day16/pretty_table.py
@@ -2,5 +2,6 @@ from prettytable import PrettyTable
 table=PrettyTable()
 table.add_column("Pokemon Name",["pakachu","Squrtle","Charmandar"])
 table.add_column("Type",["Electric","Water","Fire"])
-table.align ="r"
+#alignment
+table.align ="l"
 print(table)
