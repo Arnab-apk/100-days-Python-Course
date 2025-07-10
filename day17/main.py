c3c01c25a1f091c7eb9c962df3c9e3cf3d72830d
Arnab-apk <176360399+Arnab-apk@users.noreply.github.com>
2025-07-10 20:47:41 +0530

    done

--- day17/main.py
+++ day17/main.py
@@ -1,8 +1,33 @@
 class User:
-    pass
+    # this function gets called how many times a new object (user) is created
+        def __init__(self,user_id,username):
+            # print("new user being created!")
+            self.id=user_id
+            self.name=username
+            self.followers=0
+            self.following=0
+        #methods always contain a self parameter as its first parameter as it knows the object that it calls:
+        def follow(self,user):
+            user.followers+=1
+            user.following+=1
+            
+               
 
-user_1=User()
-user_1.id="001"
-user_1.username="arnab"
+user_1=User("001","arnab")
+# print(user_1.name)
+# print(user_1.id)
+# print(user_1.followers)
 
-print(user_1.username)
+user_2=User("002","asmi")
+# print(user_2.name)
+# print(user_2.id)
+# print(user_2.followers)
+
+user_1.follow(user_2)
+print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
+
+
+
