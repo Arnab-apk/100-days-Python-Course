93d07133fc19a74df685325465bc62e50fea6550
Arnab-apk <176360399+Arnab-apk@users.noreply.github.com>
2025-07-12 11:04:22 +0530

    done

---
+++ day18/main.py
@@ -0,0 +1,6 @@
+from turtle import Turtle,Screen
+timmy =Turtle()
+#we have to import the screen class to be able to see our graphics on a window
+#and exit when we click on the screen  
+screen=Screen()
+screen.exitonclick()
