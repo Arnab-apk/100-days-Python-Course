6c4e62224d00fc1dc642086b9434c11d1bba8e1a
Arnab-apk <176360399+Arnab-apk@users.noreply.github.com>
2025-07-13 00:21:25 +0530

    done

--- day18/spirograph.py
+++ day18/spirograph.py
@@ -4,20 +4,21 @@ random=Random()
 turtle=Turtle()
 screen=Screen()
 turtle.speed("fastest")
-turtle.width(0)
+turtle.width(1)
 def random_color():
     r = random.random()  # Random float between 0 and 1
     g = random.random()
     b = random.random()
     return (r, g, b)
-for _ in range(360):
-    turtle.color(random_color())
-    turtle.circle(100)
-    current_heading=turtle.heading()
-    turtle.setheading(current_heading+1)
-    turtle.circle(100)
-
-
+def draw_spi(size):
+    for _ in range(int(360/size)):
+        turtle.color(random_color())
+        turtle.circle(100)
+        current_heading=turtle.heading()
+        turtle.setheading(current_heading+size)
+        turtle.circle(100)
+        
+draw_spi(5)
 screen.exitonclick()
 
 #finally made something pleasing  
