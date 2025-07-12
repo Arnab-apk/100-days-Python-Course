e10c3b248525e099446c5923e63f465d6811db7f
Arnab-apk <176360399+Arnab-apk@users.noreply.github.com>
2025-07-12 11:19:08 +0530

    done

--- day18/main.py
+++ day18/main.py
@@ -1,5 +1,32 @@
 from turtle import Turtle,Screen
 timmy =Turtle()
+# to change the shape
+#stack overflow
+timmy.shape("turtle")
+timmy.color("red")
+#making a drawing using turtle
+timmy.speed(1)
+timmy.forward(100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #we have to import the screen class to be able to see our graphics on a window
 #and exit when we click on the screen  
 screen=Screen()
---
+++ day18/sq.py
@@ -0,0 +1,10 @@
+from turtle import Turtle,Screen
+turtle=Turtle()
+i=1
+while i<=4:
+    turtle.forward(100)
+    turtle.right(90)
+    i+=1
+
+display=Screen()
+display.exitonclick()
