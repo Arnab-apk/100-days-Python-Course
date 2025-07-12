1b36a23a440ea9121573d61463acc9ad8ba1d353
Arnab-apk <176360399+Arnab-apk@users.noreply.github.com>
2025-07-12 23:00:29 +0530

    done

--- day18/sq.py
+++ day18/sq.py
@@ -1,10 +1,20 @@
-from turtle import Turtle,Screen
-turtle=Turtle()
-i=1
-while i<=4:
-    turtle.forward(100)
-    turtle.right(90)
-    i+=1
+# from turtle import Turtle,Screen
+# turtle=Turtle()
+# i=1
+# while i<=4:
+#     turtle.forward(100)
+#     turtle.right(90)
+#     i+=1
 
-display=Screen()
-display.exitonclick()
+# display=Screen()
+# display.exitonclick()
+
+
+# #aliasing module
+# import turtle as t
+# tim=t.Turtle()
+
+import heroes
+print(heroes.gen())
+
+
