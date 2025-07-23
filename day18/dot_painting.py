57db46ba8b4fd49cfde84cb06551dd619a046c61
Arnab-apk <176360399+Arnab-apk@users.noreply.github.com>
2025-07-23 20:33:05 +0530

    done

--- day18/dot_painting.py
+++ day18/dot_painting.py
@@ -1,4 +1,20 @@
-from turtle import Turtle,Screen
-t=Turtle()
+import turtle as t
+import screen as s
+import random
+
+turtle=t.Turtle()
 sc=Screen()
+Turtle.colormode(255)
+def random_color():
+    r = random.random()  # Random float between 0 and 1
+    g = random.random()
+    b = random.random()
+    return (r, g, b)
+
+for _ in range(10):
+    t.dot(20,random_color())
+    t.forward(50)
+    
+sc.exitonclick()
+
 
