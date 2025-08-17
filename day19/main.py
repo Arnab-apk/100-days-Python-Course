d04e3e09c2c8d8c41a549a205b812ee39fa493d8
Arnab-apk <176360399+Arnab-apk@users.noreply.github.com>
2025-08-17 13:00:05 +0530

    done

--- day19/main.py
+++ day19/main.py
@@ -12,11 +12,16 @@ def turn_left():
 def turn_right():
     new_heading=tim.heading()-10
     tim.setheading(new_heading)
-    
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
 
 screen.listen()
 screen.onkey(move_forward,"w")
 screen.onkey(move_backward,"s")
 screen.onkey(turn_left,"a")
 screen.onkey(turn_right,"d")
+screen.onkey(clear,"c")
 screen.exitonclick()
