15de80b917197e40e4c4b0025070a95b58c0767e
Arnab-apk <176360399+Arnab-apk@users.noreply.github.com>
2025-08-25 12:15:16 +0530

    done

--- day19/race.py
+++ day19/race.py
@@ -3,9 +3,13 @@ from turtle import Turtle ,Screen
 screen=Screen()
 screen.setup(width=500,height=400)
 user_bet=screen.textinput(title="Make your Bet-",prompt="Which turtle will win the race enter the colour- ")
-print(user_bet)
-tim=Turtle(shape="turtle")
-tim.penup()
-
-tim.goto(x=-230,y=-100)
+colors=["red","orange","yellow","green","blue","purple"]
+y_pos=[-70,-40,-10,20,50,80]
+for turtle_index in range(0,6):
+    y=100
+    tim=Turtle(shape="turtle")
+    tim.penup()
+    tim.color(colors[turtle_index])
+    tim.goto(x=-230,y=y_pos[turtle_index])
+    
 screen.exitonclick()
