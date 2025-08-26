507c69cc62952542aaad7b61c55ccfb847b54c5a
Arnab-apk <176360399+Arnab-apk@users.noreply.github.com>
2025-08-26 21:42:52 +0530

    done

--- day20/snake_game/main.py
+++ day20/snake_game/main.py
@@ -5,6 +5,15 @@ screen.setup(width=600,height=600)
 screen.bgcolor("black")
 screen.title("MY SNAKE GAME")
 
+segment1=Turtle(shape="square")
+segment1.color("white")
 
+segment2=Turtle(shape="square")
+segment2.color("white")
+segment2.goto(-20,0)
+
+segment3=Turtle(shape="square")
+segment3.color("white")
+segment3.goto(-40,0)
 
 screen.exitonclick()
