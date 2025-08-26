9d2b812b9e320ac03aa54392cb1c82d20f859112
Arnab-apk <176360399+Arnab-apk@users.noreply.github.com>
2025-08-26 21:47:28 +0530

    created the snake body

--- day20/snake_game/main.py
+++ day20/snake_game/main.py
@@ -5,15 +5,20 @@ screen.setup(width=600,height=600)
 screen.bgcolor("black")
 screen.title("MY SNAKE GAME")
 
-segment1=Turtle(shape="square")
-segment1.color("white")
+starting_pos=[(0,0),(-20,0),(-40,0)]
+for position in starting_pos:
+    new_segment=Turtle("square")
+    new_segment.color("white")
+    new_segment.goto(position)
+# segment1=Turtle(shape="square")
+# segment1.color("white")
 
-segment2=Turtle(shape="square")
-segment2.color("white")
-segment2.goto(-20,0)
+# segment2=Turtle(shape="square")
+# segment2.color("white")
+# segment2.goto(-20,0)
 
-segment3=Turtle(shape="square")
-segment3.color("white")
-segment3.goto(-40,0)
+# segment3=Turtle(shape="square")
+# segment3.color("white")
+# segment3.goto(-40,0)
 
 screen.exitonclick()
