0e63714d7aea65a9fafeeb9c708ca1abc6226821
Arnab-apk <176360399+Arnab-apk@users.noreply.github.com>
2025-08-27 11:38:51 +0530

    done

--- day20/snake_game/snake.py
+++ day20/snake_game/snake.py
@@ -7,6 +7,7 @@ class Snake:
     def __init__(self):
         self.segments= []
         self.create_snake()
+        self.head=self.segments[0]
         
     def create_snake(self):
         for position in starting_pos:
@@ -21,4 +22,16 @@ class Snake:
             new_x=self.segments[seg_num-1].xcor()
             new_y=self.segments[seg_num-1].ycor()
             self.segments[seg_num].goto(new_x,new_y)
-        self.segments[0].forward(move_dist)
+        self.head.forward(move_dist)
+        
+    def up(self):
+        self.head.setheading(90)
+    def down(self):
+        self.head.setheading(-90)
+    def left(self):
+        self.head.setheading(90)
+    def right(self):
+        pass
+        
+    
+        
