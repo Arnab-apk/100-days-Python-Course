6da3e925689df15a4aa842dac6102520ecd5c843
Arnab-apk <176360399+Arnab-apk@users.noreply.github.com>
2025-08-31 12:37:16 +0530

    done

---
+++ day20/snake_game/class_inheritance/ex.py
@@ -0,0 +1,21 @@
+class animal():
+    def __init__(self):
+        self.eyes=2
+    def breath(self):
+        print("inhale exhale")
+
+class fish(animal):
+    def __init__(self):
+        super().__init__()
+    
+    def breath(self):
+        super().breath()
+        print("dooooiing itt underr watterr!")
+    
+    def swim(self):
+        print("Movving underr waterrr!") 
+           
+nemo=fish()
+nemo.breath()
+print(nemo.eyes)
+        
